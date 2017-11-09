4d2881ccfe14cf78b679cdfecb003e6200b8b7f6
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-11-09 18:58:53 -0500

    set up event listeners on board holes

--- mancala_game/app.js
+++ mancala_game/app.js
@@ -1,10 +1,30 @@
 $(() => {
 
+  // Setting up game logic for a single play
+  const playGame = (event) => {
+    console.log($(event.currentTarget));
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // Creating initial mancala board setup
   // player 1
   for (let i = 0; i < 6; i++) {
     const $hole = $('<div>').addClass('hole-1'); // creating holes (not the mancala)
     $('#row-1').append($hole); // adding holes to the mancala board
+    $hole.on('click', playGame) // adding an event listner to all the holes
     for (let j = 0; j < 4; j++) {
       const $marble = $('<div>').addClass('marble'); // creating marbles
       $hole.append($marble); // adding marbles to the mancala board
@@ -13,8 +33,9 @@ $(() => {
 
   // player 2
   for (let i = 0; i < 6; i++) {
-    const $hole = $('<div>').addClass('hole-1'); // creating holes (not the mancala)
+    const $hole = $('<div>').addClass('hole-2'); // creating holes (not the mancala)
     $('#row-2').append($hole); // adding holes to the mancala board
+    $hole.on('click', playGame) // adding an event listner to all the holes
     for (let j = 0; j < 4; j++) {
       const $marble = $('<div>').addClass('marble'); // creating marbles
       $hole.append($marble); // adding marbles to the mancala board
